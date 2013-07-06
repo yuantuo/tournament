7195626c1bc0fc222446a660551723a0a315d325
tok <tok@firstb2b.net>
2013-07-06 11:06:27 +0100

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scoreboard::Application.config.secret_token = '871ad2de5321542eb889e0f1a89ee9bf875bc2c810505b0facd16af30174f0c9e9f32fef89ab4fa1a1cf5e4f0065500a7b67b477caf4251d79492d109168e8d2'
